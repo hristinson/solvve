6fe2c4a7406091a0d5b403c01fe4cb18a0a491b0
Hristinson <92439180+hristinson@users.noreply.github.com>
2022-06-30 15:01:54 +0300

    Update homeWork_001.js

--- homeWork_001.js
+++ homeWork_001.js
@@ -2,5 +2,6 @@
 console.log(`All elements ${document.querySelectorAll(`*`).length}`)
 console.log(`DIV elements ${document.querySelectorAll(`div`).length}`)
 console.log(`Not DIV elements ${document.querySelectorAll(`*`).length - document.querySelectorAll(`div`).length}`)
-document.querySelectorAll(`p`).forEach(p => p.style.setProperty(`color`, `#ff0000`))
-
+if(document.querySelectorAll(`p`).length > 0) 
+        {document.querySelectorAll(`p`).forEach(p => p.style.setProperty(`color`, `#ff0000`)); console.log(`<p> elements are red`)} 
+                else {console.log(`no <p> elemetns on page`)}
